07c58d1a7ecc121fc8ade2adb2420b2e4b6588b0
Andreas Opferkuch <andreas.opferkuch@gmail.com>
2022-04-26 22:51:15 +0200

    Add tests

--- test/ps.js
+++ test/ps.js
@@ -74,3 +74,56 @@ test('should parse ps output on *nix', async t => {
   mockery.deregisterMock('child_process');
   mockery.deregisterMock('os');
 });
+
+test('should throw if stderr contains an error', async t => {
+  const stdout =
+    'PPID   PID\n' +
+    '   1   430\n' +
+    ' 430   432\n' +
+    '   1   727\n' +
+    '   1  7166\n';
+
+  mockery.registerMock('child_process', {
+    spawn: () => mocks.spawn(stdout, 'Some error', null, 0, null),
+  });
+  mockery.registerMock('os', {
+    EOL: '\n',
+    platform: () => 'linux',
+    type: () => 'type',
+    release: () => 'release',
+  });
+
+  const ps = require('../lib/ps');
+
+  await t.throws(pify(ps)())
+
+  mockery.deregisterMock('child_process');
+  mockery.deregisterMock('os');
+});
+
+test('should not throw if stderr contains the "bogus" error message from vscode', async t => {
+  const stdout =
+    'PPID   PID\n' +
+    '   1   430\n' +
+    ' 430   432\n' +
+    '   1   727\n' +
+    '   1  7166\n';
+
+  mockery.registerMock('child_process', {
+    spawn: () => mocks.spawn(stdout, 'Error: your 131072x1 screen size is bogus. expect trouble', null, 0, null),
+  });
+  mockery.registerMock('os', {
+    EOL: '\n',
+    platform: () => 'linux',
+    type: () => 'type',
+    release: () => 'release',
+  });
+
+  const ps = require('../lib/ps');
+
+  const result = await pify(ps)();
+  t.deepEqual(result, [[1, 430], [430, 432], [1, 727], [1, 7166]]);
+
+  mockery.deregisterMock('child_process');
+  mockery.deregisterMock('os');
+});
